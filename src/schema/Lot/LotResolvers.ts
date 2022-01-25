7d598cf205537ce9b8e70cda7978ef04b556ca79
Morgan Doane <morganddoane@gmail.com>
2022-01-25 13:29:10 -0700

    lot resolvers

--- src/schema/Lot/LotResolvers.ts
+++ src/schema/Lot/LotResolvers.ts
@@ -1,5 +1,3 @@
-import { FulfillmentModel } from './../Fulfillment/Fulfillment';
-import { BolModel } from './../Bol/Bol';
 import { LotFinder } from './LotFinder';
 import { Permitted } from '@src/auth/middleware/Permitted';
 import { createBaseResolver } from './../Base/BaseResolvers';
@@ -24,29 +22,6 @@ const BaseResolvers = createBaseResolver();
 
 @Resolver(() => Lot)
 export class LotResolvers extends BaseResolvers {
-    @Mutation(() => Boolean)
-    async cleanUpLots(): Promise<boolean> {
-        const matches = await LotModel.find({
-            fulfillment_type: { $exists: true },
-        });
-
-        let count = 0;
-
-        for (const match of matches) {
-            const fulfillment = await FulfillmentModel.findOne({
-                lots: match._id,
-            });
-            if (fulfillment) {
-                if (!match.company) match.company = fulfillment.company;
-                if (!match.location) match.location = fulfillment.location;
-                await match.save();
-                count += 1;
-                console.log(`${count}/${matches.length}`);
-            }
-        }
-        return true;
-    }
-
     @UseMiddleware(
         Permitted({ type: 'permission', permission: Permission.GetLots })
     )
